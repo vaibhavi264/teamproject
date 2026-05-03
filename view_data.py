bb9c217bca6a4c267c906f517ae48404f5159ea8
Rushikesh <your-email@gmail.com>
2026-05-04 03:34:44 +0530

    5 commit

--- view_data.py
+++ view_data.py
@@ -1,12 +1,12 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://postgres:password@containers-us-west-xx.railway.app:6543/railway")
 
 # If running locally, paste your Railway DATABASE_URL manually:
 # DATABASE_URL = "postgresql://user:password@host:port/dbname"
 
-conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(postgresql://postgres:password@containers-us-west-xx.railway.app:6543/railway)
 cur = conn.cursor()
 
 print("USERS:")
